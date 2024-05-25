bd56ada070af7f0b0987661685b74cad413b4828
Pham Ba Cong <congphamba2005@gmail.com>
2024-05-25 16:41:54 +0700

    upload files

---
+++ train.py
@@ -0,0 +1,83 @@
+import torch.nn as nn
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+from dataset import causal_mask, BilingualDataset
+from model import build_transformer
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translation'][lang]
+
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(
+            get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
+
+def get_ds(config):
+    ds_raw = load_dataset(
+        'opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+
+    # build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # keep 90% for train and 10% for validation
+    train_ds_size = int(0.9*len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(
+        ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt,
+                                config["lang_src"], config["lang_tgt"], config["seq_len"])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt,
+                              config["lang_src"], config["lang_tgt"], config["seq_len"])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']])
+        tgt_ids = tokenizer_src.encode(item['translation'][config['lang_tgt']])
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max len of source sentence: {max_len_src}')
+    print(f'Max len of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(
+        train_ds, batch=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
+    return model
+
+def train_model(config):
+    #define the devie
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'using device {device}')
+    
